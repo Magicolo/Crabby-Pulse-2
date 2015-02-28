9682632ec0283aa58b6cbc7584f92da177dd0f47
Magicolo <straty91@hotmail.com>
2015-02-28 16:04:45 -0500

    pushing

---
+++ Project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Magicolo;
+
+public class AudioManager : MonoBehaviourExtended {
+
+	void Awake() {
+		PureData.OpenPatch("main");
+	}
+	
+	void Update() {
+		float xAxis = Input.GetAxisRaw("Horizontal");
+		float yAxis = Input.GetAxisRaw("Vertical");
+		bool trigger = Input.GetKey(KeyCode.JoystickButton0);
+		PureData.Send("x_axis", xAxis);
+		PureData.Send("y_axis", yAxis);
+		PureData.Send("trigger", trigger);
+	}
+}
